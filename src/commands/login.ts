ddd13ba254f08e884098cfed036e4826ec77561a
Robin Genz <mail@robingenz.dev>
2024-04-03 17:42:23 +0200

    feat(login): improve error message

--- src/commands/login.ts
+++ src/commands/login.ts
@@ -12,19 +12,26 @@ export default defineCommand({
   run: async (ctx) => {
     const email = await consola.prompt('Email:', { type: 'text' })
     const password = await consola.prompt('Password:', { type: 'text' })
-    const sessionResponse = await axios.post<{ id: string }>(`${API_URL}/sessions`, {
-      email: email,
-      password: password
-    })
     consola.start('Logging in...')
+    let sessionId: string;
+    try {
+      const sessionResponse = await axios.post<{ id: string }>(`${API_URL}/sessions`, {
+        email: email,
+        password: password
+      })
+      sessionId = sessionResponse.data.id
+    } catch (error) {
+      consola.error('Invalid email or password.')
+      return
+    }
     const tokenResponse = await axios.post<{ token: string }>(`${API_URL}/tokens`,
       { name: 'Capawesome CLI' },
-      { headers: { Authorization: `Bearer ${sessionResponse.data.id}` } }
+      { headers: { Authorization: `Bearer ${sessionId}` } }
     )
     userConfig.write({
       username: email,
       token: tokenResponse.data.token
     })
-    consola.success(`Successfully logged in as ${email}!`)
+    consola.success(`Successfully signed in!`)
   },
 })
