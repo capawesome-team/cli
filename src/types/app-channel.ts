32994f2963be61e08aac8deace4ea3e56c8ef2cb
Robin Genz <mail@robingenz.dev>
2024-11-01 12:27:31 +0100

    feat(apps): add `--bundle-limit` option

--- src/types/app-channel.ts
+++ src/types/app-channel.ts
@@ -5,6 +5,7 @@ export interface AppChannelDto {
 export interface CreateAppChannelDto {
   appId: string;
   name: string;
+  totalAppBundleLimit?: number;
 }
 
 export interface DeleteAppChannelDto {
