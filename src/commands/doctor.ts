1d9d0acf74b1b3ce63c87031a8499227bf3e3f8b
Nico Genz <34741282+nicogenz@users.noreply.github.com>
2025-03-24 15:13:02 +0100

    feat(doctor): add doctor command (#37)

---
+++ src/commands/doctor.ts
@@ -0,0 +1,23 @@
+import { defineCommand } from 'citty';
+import consola from 'consola';
+import pkg from '../../package.json';
+import systeminformation from 'systeminformation';
+
+export default defineCommand({
+  meta: {
+    name: 'doctor',
+    description: 'Prints out neccessary information for debugging',
+  },
+  run: async () => {
+    const osInfo = await systeminformation.osInfo();
+    const versions = await systeminformation.versions('npm, node');
+    consola.box(
+      [
+        `NodeJS version: ${versions.node}`,
+        `NPM version: ${versions.npm}`,
+        `CLI version: ${pkg.version}`,
+        `OS: ${osInfo.distro} ${osInfo.release} ${osInfo.codename ? `(${osInfo.codename})` : ''}`,
+      ].join('\n'),
+    );
+  },
+});
