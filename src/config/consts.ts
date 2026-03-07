3df8e982d866ee73c05931ad5368aeca6ec3daf0
Robin Genz <mail@robingenz.dev>
2026-03-07 13:49:22 +0100

    chore: update `MAX_CONCURRENT_PART_UPLOADS` to 4

--- src/config/consts.ts
+++ src/config/consts.ts
@@ -2,4 +2,4 @@ export const DEFAULT_API_BASE_URL = 'https://api.cloud.capawesome.io';
 export const DEFAULT_CONSOLE_BASE_URL = 'https://console.cloud.capawesome.io';
 export const MANIFEST_JSON_FILE_NAME = 'capawesome-live-update-manifest.json'; // Do NOT change this!
 export const MAX_CONCURRENT_FILE_UPLOADS = 20;
-export const MAX_CONCURRENT_PART_UPLOADS = 3;
+export const MAX_CONCURRENT_PART_UPLOADS = 4;
