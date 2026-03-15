50040958cd2a873ff296981b0950b3a799505a93
Robin Genz <mail@robingenz.dev>
2026-03-15 08:30:09 +0100

    feat(update): add dev version detection in update check (#128)
    * feat(update): add dev version detection in update check
    
    * remove console log
    
    * wip

--- src/services/update.ts
+++ src/services/update.ts
@@ -1,10 +1,10 @@
-import { createRequire } from 'module';
+import { NpmPackageDto } from '@/types/npm-package.js';
+import httpClient, { HttpClient } from '@/utils/http-client.js';
 import consola from 'consola';
+import { createRequire } from 'module';
 import * as semver from 'semver';
 const require = createRequire(import.meta.url);
 const pkg = require('../../package.json');
-import { NpmPackageDto } from '@/types/npm-package.js';
-import httpClient, { HttpClient } from '@/utils/http-client.js';
 
 export interface UpdateService {
   checkForUpdate(): Promise<void>;
@@ -21,6 +21,13 @@ class UpdateServiceImpl implements UpdateService {
     try {
       const response = await this.httpClient.get<NpmPackageDto>(`https://registry.npmjs.org/${pkg.name}/latest`);
       const latestVersion = response.data.version;
+      if (pkg.version.includes('-dev')) {
+        console.log(''); // Add an empty line for better readability
+        consola.info(
+          `You are using a development version of Capawesome CLI (${pkg.version}). The latest stable version is ${latestVersion}.`,
+        );
+        return;
+      }
       if (semver.gt(latestVersion, pkg.version)) {
         consola.warn(
           `New version of Capawesome CLI available: ${pkg.name}@${latestVersion}. Please update to receive the latest features and bug fixes.`,
